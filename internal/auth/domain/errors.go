7cba262418547d52c045d772afd5a5cc0161b59f
AriartyyyA <dodarov42@gmail.com>
2026-04-30 00:37:04 +0300

    создал базовые ошибки

--- internal/auth/domain/errors.go
+++ internal/auth/domain/errors.go
@@ -1,1 +1,9 @@
 package domain
+
+import "errors"
+
+var (
+	ErrUserNotFound  = errors.New("user not found")
+	ErrUserExists    = errors.New("user already exists")
+	ErrWrongPassword = errors.New("wrong password")
+)
