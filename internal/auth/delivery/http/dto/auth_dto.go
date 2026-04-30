a6fc2421cdbefb9d9f484fc871bdc95eb66fa187
AriartyyyA <dodarov42@gmail.com>
2026-04-30 14:37:21 +0300

    добавил хэндер login

--- internal/auth/delivery/http/dto/auth_dto.go
+++ internal/auth/delivery/http/dto/auth_dto.go
@@ -4,3 +4,7 @@ type LoginAndRegisterRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
--- internal/auth/delivery/http/handler.go
+++ internal/auth/delivery/http/handler.go
@@ -28,7 +28,7 @@ func NewHandlerAuth(uc AuthUserCase) *HandlerAuth {
 
 func (h *HandlerAuth) RegisterRoutes(router chi.Router) {
 	router.Post("/auth/register", h.Register)
-	// router.Post("/auth/login", h.Login)
+	router.Post("/auth/login", h.Login)
 }
 
 func (h *HandlerAuth) Register(w http.ResponseWriter, r *http.Request) {
@@ -54,4 +54,41 @@ func (h *HandlerAuth) Register(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+}
+
+func (h *HandlerAuth) Login(w http.ResponseWriter, r *http.Request) {
+	var reqDto dto.LoginAndRegisterRequest
+	if err := json.NewDecoder(r.Body).Decode(&reqDto); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	token, err := h.uc.Login(r.Context(), reqDto.Email, reqDto.Password)
+	if err != nil {
+		if errors.Is(err, domain.ErrUserNotFound) {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(map[string]string{"error": "user not found"})
+			return
+		}
+		if errors.Is(err, domain.ErrWrongPassword) {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	resp := dto.LoginResponse{
+		Token: token,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 }
