19597c63f18e0397da04d6f6d0530bd13338b0e5
AriartyyyA <dodarov42@gmail.com>
2026-04-30 17:34:57 +0300

    добавил domain слой для микрача wallet

---
+++ internal/wallet/domain/entity.go
@@ -0,0 +1,31 @@
+package domain
+
+import (
+	"context"
+	"time"
+)
+
+type Wallet struct {
+	ID        string
+	UserID    string
+	Balance   int64
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Transaction struct {
+	ID           string
+	FromWalletID string
+	ToWalletID   string
+	Amount       int64
+	CreatedAt    time.Time
+}
+
+type WalletRepository interface {
+	CreateWallet(ctx context.Context, wallet Wallet) error
+	FindWalletByID(ctx context.Context, walletID string) (*Wallet, error)
+	FindWalletByUserID(ctx context.Context, userID string) (*Wallet, error)
+	UpdateBalance(ctx context.Context, walletID string, amount int64) (int64, error)
+	CreateTransaction(ctx context.Context, transaction Transaction) error
+	GetTransactionsByWalletID(ctx context.Context, walletID string) ([]*Transaction, error)
+}
---
+++ internal/wallet/domain/errors.go
@@ -0,0 +1,10 @@
+package domain
+
+import "errors"
+
+var (
+	ErrWalletNotFound            = errors.New("wallet not found")
+	ErrNoMoney                   = errors.New("low balance")
+	ErrTransactionNegativeAmount = errors.New("negative amount")
+	ErrToSendMyself              = errors.New("to send amount myself")
+)
