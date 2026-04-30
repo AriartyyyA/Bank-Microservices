24f563d7a51fd75978c402b1599157508fa0d03a
AriartyyyA <dodarov42@gmail.com>
2026-04-30 12:15:50 +0300

    написал фукцию по получению пользователя с помощью почты

--- internal/auth/repository/pg/repository.go
+++ internal/auth/repository/pg/repository.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 
 	"github.com/AriartyyyA/gobank/internal/auth/domain"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -44,7 +45,26 @@ func (r *PostgresRepo) CreateUser(ctx context.Context, user domain.User) error {
 }
 
 func (r *PostgresRepo) GetUserByEmail(ctx context.Context, email string) (*domain.User, error) {
-	return nil, nil
+	query := `SELECT id, email, password_hash, created_at, updated_at FROM users WHERE email = $1`
+
+	row := r.connPool.QueryRow(ctx, query, email)
+
+	var user domain.User
+	if err := row.Scan(
+		&user.UUID,
+		&user.Email,
+		&user.PasswordHash,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	); err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, domain.ErrUserNotFound
+		}
+
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func (r *PostgresRepo) GetUserByID(ctx context.Context, id string) (*domain.User, error) {
