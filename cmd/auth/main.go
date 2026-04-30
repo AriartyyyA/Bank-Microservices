9103763184caf5c40d1fb19d8c52b260506ea298
AriartyyyA <dodarov42@gmail.com>
2026-04-30 15:06:50 +0300

    добавил базовую реализаию запуска приложения

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
 
+	transport "github.com/AriartyyyA/gobank/internal/auth/delivery/http"
+	pg_repo "github.com/AriartyyyA/gobank/internal/auth/repository/pg"
+	"github.com/AriartyyyA/gobank/internal/auth/usecase"
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+}
+
+func main() {
+	pool, err := pgxpool.New(context.Background(), os.Getenv("DB_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repo := pg_repo.NewPostgresRepo(pool)
+	uc := usecase.NewAuthUseCase(repo, os.Getenv("JWT_SECRET"))
+	handlers := transport.NewHandlerAuth(uc)
+
+	router := chi.NewRouter()
+	handlers.RegisterRoutes(router)
+
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal(err)
+	}
 }
