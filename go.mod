e36a2f2fb27a2ea791142f5cfca0e768b588cf1c
AriartyyyA <dodarov42@gmail.com>
2026-05-06 13:47:25 +0300

    fix auth tests

--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/alicebob/miniredis/v2 v2.37.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
@@ -43,6 +44,7 @@ require (
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
+	github.com/yuin/gopher-lua v1.1.1 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/mod v0.34.0 // indirect
 	golang.org/x/net v0.52.0 // indirect
--- internal/auth/usecase/usecase_test.go
+++ internal/auth/usecase/usecase_test.go
@@ -8,10 +8,19 @@ import (
 	"github.com/AriartyyyA/gobank/internal/auth/domain"
 	"github.com/AriartyyyA/gobank/internal/auth/domain/mocks"
 	"github.com/AriartyyyA/gobank/internal/auth/usecase"
+	"github.com/alicebob/miniredis/v2"
+	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"golang.org/x/crypto/bcrypt"
 )
+
+func setupRedis(t *testing.T) *redis.Client {
+	mr := miniredis.RunT(t)
+	return redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
+}
 
 func TestAuthUseCase_Register_Success(t *testing.T) {
 	mockRepo := mocks.NewUserRepository(t)
@@ -21,7 +30,7 @@ func TestAuthUseCase_Register_Success(t *testing.T) {
 
 	mockRepo.On("CreateUser", mock.Anything, mock.Anything).Return(nil)
 
-	uc := usecase.NewAuthUseCase(mockRepo, "secret")
+	uc := usecase.NewAuthUseCase(mockRepo, "secret", setupRedis(t))
 	err := uc.Register(context.Background(), "test@test.com", "password123")
 
 	assert.NoError(t, err)
@@ -32,7 +41,7 @@ func TestAuthUseCase_Register_AlreadyExists(t *testing.T) {
 
 	mockRepo.On("GetUserByEmail", mock.Anything, "test@test.com").Return(&domain.User{}, nil)
 
-	uc := usecase.NewAuthUseCase(mockRepo, "secret")
+	uc := usecase.NewAuthUseCase(mockRepo, "secret", setupRedis(t))
 	err := uc.Register(context.Background(), "test@test.com", "password123")
 
 	assert.ErrorIs(t, err, domain.ErrUserExists)
@@ -43,7 +52,7 @@ func TestAuthUseCase_Register_DBError(t *testing.T) {
 
 	mockRepo.On("GetUserByEmail", mock.Anything, "test@test.com").Return(nil, errors.New("db error"))
 
-	uc := usecase.NewAuthUseCase(mockRepo, "secret")
+	uc := usecase.NewAuthUseCase(mockRepo, "secret", setupRedis(t))
 	err := uc.Register(context.Background(), "test@test.com", "password123")
 
 	assert.Error(t, err)
@@ -61,7 +70,7 @@ func TestAuthUseCase_Login_Success(t *testing.T) {
 
 	mockRepo.On("GetUserByEmail", mock.Anything, "test@test.com").Return(user, nil)
 
-	uc := usecase.NewAuthUseCase(mockRepo, "secret")
+	uc := usecase.NewAuthUseCase(mockRepo, "secret", setupRedis(t))
 	accessToken, refreshToken, err := uc.Login(context.Background(), "test@test.com", "password123")
 
 	assert.NoError(t, err)
@@ -74,7 +83,7 @@ func TestAuthUserCase_Login_UserNotFound(t *testing.T) {
 
 	mockRepo.On("GetUserByEmail", mock.Anything, "test@test.com").Return(nil, domain.ErrUserNotFound)
 
-	uc := usecase.NewAuthUseCase(mockRepo, "secret")
+	uc := usecase.NewAuthUseCase(mockRepo, "secret", setupRedis(t))
 	_, _, err := uc.Login(context.Background(), "test@test.com", "password123")
 
 	assert.ErrorIs(t, err, domain.ErrUserNotFound)
@@ -91,8 +100,8 @@ func TestAuthUserCase_Login_WrongPassword(t *testing.T) {
 	mockRepo := mocks.NewUserRepository(t)
 	mockRepo.On("GetUserByEmail", mock.Anything, "test@test.com").Return(user, nil)
 
-	uc := usecase.NewAuthUseCase(mockRepo, "secret")
-	_, _, err := uc.Login(context.Background(), "test@test.com", "password123")
+	uc := usecase.NewAuthUseCase(mockRepo, "secret", setupRedis(t))
+	_, _, err := uc.Login(context.Background(), "test@test.com", "wrongpassword")
 
 	assert.ErrorIs(t, err, domain.ErrWrongPassword)
 }
